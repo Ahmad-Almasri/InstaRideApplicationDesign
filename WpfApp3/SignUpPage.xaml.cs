f49b11edf4b06e82afa960704c8eae3db782c168
Ahmad Almasri <ahmad.almasri3@ucalgary.ca>
2021-12-02 15:15:28 -0700

    Update

--- WpfApp3/SignUpPage.xaml.cs
+++ WpfApp3/SignUpPage.xaml.cs
@@ -23,7 +23,38 @@ namespace WpfApp3
         public SignUpPage()
         {
             InitializeComponent();
+            addDayToComboBox();
+            addMonthToComboBox();
+            addYearToCmboBox();
         }
+
+        private void addYearToCmboBox()
+        {
+            for (int i = 2004; i >= 1950; i--)
+            {
+                comboBoxYears.Items.Add(i);
+            }
+        }
+
+        private void addMonthToComboBox()
+        {
+            string[] months = System.Globalization.CultureInfo.CurrentCulture.DateTimeFormat.MonthNames;
+
+            foreach (string month in months) // writing out
+            {
+                comboBoxMonths.Items.Add(month);
+            }
+        }
+
+        private void addDayToComboBox()
+        {
+            for (int i = 1; i <= 31; i++)
+            {
+                comboBoxDays.Items.Add(i);
+            }
+        }
+
+        
 
         private void ActiveTicketDown(object sender, MouseButtonEventArgs e)
         {
